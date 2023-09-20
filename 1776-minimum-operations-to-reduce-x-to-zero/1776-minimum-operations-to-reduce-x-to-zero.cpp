c6e9d41589fe5d9532ee5aa18b5bda85eddeb906
Utkarsh Gupta <104654053+Utkarsh7772@users.noreply.github.com>
2023-09-20 23:16:00 +0530

    Time: 128 ms (54.41%), Space: 98.9 MB (35.28%) - LeetHub

---
+++ 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+        int total = std::accumulate(nums.begin(), nums.end(), 0);
+        int target = total - x;
+        int left = 0;
+        int n = nums.size();
+        int maxLength = -1;
+        int runningSum = 0;
+
+        for (int right = 0; right < n; right++) {
+            runningSum += nums[right];
+
+            while (runningSum > target && left <= right) {
+                runningSum -= nums[left];
+                left++;
+            }
+
+            if (runningSum == target) {
+                maxLength = std::max(maxLength, right - left + 1);
+            }
+        }
+        return maxLength != -1 ? n - maxLength : -1;
+        }
+};
