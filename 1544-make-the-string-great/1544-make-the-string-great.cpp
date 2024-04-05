8b36f6b054e382490dd2ca36e53c6f954a914e93
Utkarsh Gupta <104654053+Utkarsh7772@users.noreply.github.com>
2024-04-05 12:39:14 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (64.31%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        string ans="";
+        for(int i=0;i<s.size();i++)
+        {
+             if (!ans.empty() && abs(s[i] - ans.back()) == 32) {
+                ans.pop_back(); 
+            } 
+            else
+            {
+                ans.push_back(s[i]); 
+            }
+        }
+        return ans;
+    }
+};
