2341a7cc1abaa13b89c88618e6ddca269b2045d7
Utkarsh Gupta <104654053+Utkarsh7772@users.noreply.github.com>
2024-06-25 17:22:19 +0530

    Time: 10 ms (45.63%), Space: 8.6 MB (62.29%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,18 +3,8 @@ public:
     bool isAnagram(string s, string t) {
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
-        int n=s.size();
-        int m=t.size();
-        if(n!=m)
+        if(s==t)
+            return true;
         return false;
-        for(int i=0;i<n;i++)
-        {
-            if(s[i]==t[i])
-            {
-                continue;
-            }
-            return false;
-        }
-        return true;
     }
 };
