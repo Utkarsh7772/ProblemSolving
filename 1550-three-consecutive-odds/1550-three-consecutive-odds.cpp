20fae67dffdb98f3219e593e05d3503d29b468b2
Utkarsh Gupta <104654053+Utkarsh7772@users.noreply.github.com>
2024-07-02 01:54:39 +0530

    Time: 6 ms (25.23%), Space: 10.6 MB (8.28%) - LeetHub

---
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int n=arr.size();
+         int cnt = 0;
+        for(int num : arr) {
+            if(num % 2 != 0) {
+                cnt++;
+                if(cnt == 3) { 
+                    return true; 
+                }
+            } else {
+                cnt = 0; 
+            }
+        }
+        return false; 
+    }
+};
